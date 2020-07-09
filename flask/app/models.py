6bf508975b1311f5bfafb6e19d9ecc0e0cff60f8
c2000e <christianermann@gmail.com>
2020-07-09 13:45:17 -0600

    Added deadline status to model

--- flask/app/models.py
+++ flask/app/models.py
@@ -1,18 +1,22 @@
-from app import db
-from datetime import datetime
-from sqlalchemy.dialects.postgresql import JSON
+import datetime
 
-class Deadline(db.Model):
+from app import db
+
+def status_default(context):
+    if context.get_current_parameters()['date'] < datetime.datetime.today():
+        return 'past due'
+    return 'in progress'
+
+class DeadlineModel(db.Model):
     __tablename__ = 'deadlines'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32))
     date = db.Column(db.DateTime)
+    status = db.Column(db.String(32), default=status_default)
 
-    def __init__(self, name, date):
+    def __init__(self, name, date, status=None):
         self.name = name
         self.date = date
-
-    def __repr__(self):
-        return '<id {}>'.format(self.id)
+        self.status = status
 
